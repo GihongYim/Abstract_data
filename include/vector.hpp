f1e60d6ee469dcbc3d38bfe6be3dc80a639e45be
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-04-08 22:01:48 +0900

    asdf

--- include/vector.hpp
+++ include/vector.hpp
@@ -89,9 +89,110 @@ protected:
     void _insert_aux(iterator __position); 
 
 public:
-    iterator    begin() { return iterator (_start); }
-    iterator    begin() const { return iterator (_start); }
-    iterator    finish() { return iterator(_finish)}
+    iterator    begin() 
+        { return iterator (_start); }
+
+    const_iterator    begin() const
+        { return const_iterator (_start); }
+
+    iterator    end() 
+        { return iterator (_finish); }
+
+    const_iterator    end() const
+    { return const_iterator(_finish)}
+
+
+    reverse_iterator    rbegin()
+        { return reverse_iterator(end())};
+
+    const_reverse_iterator    rbegin() const
+        { return const_reverse_iterator(end())};
+
+    reverse_iterator    rend()
+        { return reverse_iterator(begin())};
+
+    const_reverse_iterator    rend() const
+        { return const_reverse_iterator(begin())};
+
+
+    size_type   size() const 
+        { return size_type(end() - begin());}
+    
+    size_type   max_size() const
+        { return size_type(-1) / sizeof(_Tp); }
+    
+    size_type   capacity() const
+        { return size_type(const_iterator(_end_of_storage) - begin()); }
+    
+
+    bool empty() const
+        { return begin() == end();}
+
+    reference   operator[] (size_type __n) { return *(begin() + __n); }
+
+    const_reference   operator[] (size_type __n) const { return *(begin() + __n); }
+
+    void    _range_check(size_type __n) const {
+        if (__n >= this->size()) 
+            _throw_out_of_range("vector");
+    }
+
+    reference   at(size_type __n)
+        {_range_check(__n) return (*this)[__n];}
+
+    const_reference   at(size_type __n) const
+        {_range_check(__n) return (*this)[__n];}
+
+    
+    explicit vector(const allocator_type& __a = allocator_type()) 
+        : _base(__a){ }
+
+    vector(size_type __n, const _Tp& __value,
+        const allocator_type& __a = allocator_type())
+        : _base(__n, __a)
+        { _finish = uninitialized_fill_n(_start, __n, __value); }
+    
+    explicit    vector(size_type __n)
+        : _base(__n, allocator_type())
+        { _finish = uninitialized_fill_n(_start, __n, _Tp()); }
+
+    vector(const vector<_Tp, Alloc>& __x)
+        : _base(__x.size(), __x.get_allocator())
+        { _finish = uninitialized_copy(__x.begin(), __x.end(), _start); }
+    
+    template <class _InputIterator>
+        vector(_InputIterator __first, _InputIterator __last,
+            const allocator_type& __a = allocator_type())
+            : _base(_a)
+            {
+                typedef typename _Is_integer<_InputIterator>::_Integral _Integral;
+                _initialize_aux(__first, __last, _Integral());
+            }
+    
+    template <class _Integer>
+        void _initialize_aux(_Integer __n, _Integer __value, __true_type)
+        {
+            _start = _allocate(__n);
+            _end_of_storage = _start + __n;
+            _finish = uninitialized_fill_n(_start, __n, __value);
+        }
+
+    template<class _InputIterator>
+        void _initialize_aux(_InputIterator __first, _InputIterator __last, __false_type)
+        {
+            typedef typename iterator_trait<_InputIterator>::iterator_category  _IterCategory;
+            _range_initialize(__first, __last, _IterCategory());
+        }
+    
+    ~vector()
+    { _Destroy(_start, _finish); }
+    
+    vector<_Tp, _Alloc>& operator=(const vector<_Tp, _Alloc>& __x);
+
+    void reverse(size_type __n) {
+        
+    }
+
 };
 
 }
