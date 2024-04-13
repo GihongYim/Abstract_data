83a1de6a2a43746b948329c7e5fff242f461c178
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-04-13 19:42:18 +0900

    iterator.hpp

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -2,13 +2,39 @@
 
 namespace ft
 {
-    template<
-        class Category,
-        class T,
-        class Distance = std::ptrdiff_t,
-        class Pointer = T*,
-        class Reference = T&
-    >
+template<class Iter>
+    struct iterator_traits {
+        typedef Iter::difference_type   difference_type;
+        typedef Iter::value_type        value_type;
+        typedef Iter::pointer           pointer;
+        typedef Iter::reference         reference;
+        typedef Iter::iterator_category iterator_category;
+    };
+
+template<class T>
+    struct iterator_traits<T*> {
+        typedef std::ptrdiff_t                  difference_type;
+        typedef T                               value_type;
+        typedef T*                              pointer;
+        typedef T&                              reference;
+        typedef ft::random_access_iterator_tag  iterator_category;
+    };
+template<class T>
+    struct iterator_traits<const T*> {
+        typedef std::ptrdiff_t                  difference_type;
+        typedef T                               value_type;
+        typedef const T*                        pointer;
+        typedef const T&                        reference;
+        typedef ft::random_access_iterator_tag  iterator_category;
+    };
+
+template<
+    class Category,
+    class T,
+    class Distance = std::ptrdiff_t,
+    class Pointer = T*,
+    class Reference = T&
+>
     struct iterator
     {
         typedef Category    iterator_category;
