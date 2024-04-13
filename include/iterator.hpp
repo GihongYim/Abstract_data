632a0f564676863f83be02fc99fa3340925b1db7
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-04-13 16:35:25 +0900

    iterator

---
+++ include/iterator.hpp
@@ -0,0 +1,22 @@
+#include <cstddef>
+
+namespace ft
+{
+    template<
+        class Category,
+        class T,
+        class Distance = std::ptrdiff_t,
+        class Pointer = T*,
+        class Reference = T&
+    >
+    struct iterator
+    {
+        typedef Category    iterator_category;
+        typedef T           value_type;
+        typedef _Distance   difference_type;
+        typedef _Pointer    pointer;
+        typedef _Reference  reference;
+    };
+    
+
+}
